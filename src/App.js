af7702be2d19f996b6a7160d46e728480b19d2b1
veeranna-ahana <bedasur.veeranna@ahana.co.in>
2024-10-04 16:44:36 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+/** @format */
+
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Auth/Login";
 import Home from "./pages/Home";
 import HomeOne from "./pages/HomeOne";
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  return (
-   <div>
-    <ToastContainer />
-     <BrowserRouter>
-      <Routes>
-        <Route element={<Login />} path="/" />
-        <Route path="/home" element={<Home/>} />
-        {/* <Route path="/home" element={<Home />} /> */}
-        <Route path="/salesHome" element={<HomeOne />} />
-      </Routes>
-    </BrowserRouter>
-   </div>
-  );
+	return (
+		<div>
+			<ToastContainer />
+			<BrowserRouter>
+				<Routes>
+					<Route
+						element={<Login />}
+						path="/"
+					/>
+					<Route
+						path="/home"
+						element={<Home />}
+					/>
+					{/* <Route path="/home" element={<Home />} /> */}
+					<Route
+						path="/salesHome"
+						element={<HomeOne />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
